434128c07f1e47387fc1c4c6bacd44abf44bfdd5
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2024-02-01 18:14:34 -0800

    Binary Trees

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the binary tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    if (tree->left == NULL && tree->right == NULL)
+        return 1;
+
+    if (tree->left != NULL && tree->right != NULL)
+        return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+
+    return 0;
+}
